247a91b2ce717a5340f8720bc7614985beb820b5
Ansear <sebastianarciniegas2003@gmail.com>
2023-12-04 18:03:50 -0500

    feat: :tada: Init Repository

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int result = 0;
+        Console.WriteLine("Ingrese Multiplicador");
+        int multiplica = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine("Ingrese Multiplicando");
+        int multiplican = Convert.ToInt16(Console.ReadLine());
+        while (multiplica != 1)
+        {
+            if (multiplica % 2 != 0)
+            {
+                result += multiplican;
+            }
+            multiplican *= 2;
+            multiplica /= 2;
+        }
+        result += multiplican;
+        Console.WriteLine("Resultado: " + result);
+        Console.ReadKey();
+
+    }
+}   
---
+++ README.md
@@ -0,0 +1,5 @@
+# MULTIPLICACION RUSA
+- Desarrolle un programa que reciba como entrada el multiplicador y el multiplicando, y entregue
+como resultado el producto de ambos, calculado mediante el método de multiplicación rusa.
+
+- - Se inicia el programa con _dotnet run_, Luego se pide el multiplicador y despues el multiplicando
